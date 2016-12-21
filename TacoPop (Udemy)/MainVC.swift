82d10ee34385c293eb052692fcf82a1d8a4356a0
Mahmoud333 <Mahmoud@mahmouds-mini.zte.com.cn>
2016-12-21 18:14:11 +0200

    first commit

---
+++ TacoPop (Udemy)/MainVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainVC.swift
+//  TacoPop (Udemy)
+//
+//  Created by Mahmoud Hamad on 12/21/16.
+//  Copyright © 2016 Mahmoud SMGL. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
